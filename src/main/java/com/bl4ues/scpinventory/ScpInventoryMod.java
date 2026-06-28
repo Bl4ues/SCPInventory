d2088831c72a8d79a2fd4d7b2cd7574322d64b2a
Deyvid Campos <deyvid04campos@gmail.com>
2026-06-27 21:01:35 -0300

    Register SCP inventory common config

--- src/main/java/com/bl4ues/scpinventory/ScpInventoryMod.java
+++ src/main/java/com/bl4ues/scpinventory/ScpInventoryMod.java
@@ -1,9 +1,12 @@
 package com.bl4ues.scpinventory;
 
+import com.bl4ues.scpinventory.config.ScpInventoryConfig;
 import com.bl4ues.scpinventory.network.ModNetwork;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(ScpInventoryMod.MODID)
@@ -14,11 +17,11 @@ public class ScpInventoryMod {
     public ScpInventoryMod() {
         IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
         modBus.addListener(this::commonSetup);
+
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, ScpInventoryConfig.SPEC);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
-        event.enqueueWork(() -> {
-            ModNetwork.register();
-        });
+        event.enqueueWork(ModNetwork::register);
     }
 }
