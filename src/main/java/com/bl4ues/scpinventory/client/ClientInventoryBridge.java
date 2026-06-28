d7f6cbe1848bdae51e04058b9a55f4bba7c25893
Deyvid Campos <deyvid04campos@gmail.com>
2026-06-28 20:57:24 -0300

    Expose client drag move helpers

--- src/main/java/com/bl4ues/scpinventory/client/ClientInventoryBridge.java
+++ src/main/java/com/bl4ues/scpinventory/client/ClientInventoryBridge.java
@@ -3,6 +3,7 @@ package com.bl4ues.scpinventory.client;
 import com.bl4ues.scpinventory.item.ScpEquipmentSlot;
 import com.bl4ues.scpinventory.network.EquipmentActionPacket;
 import com.bl4ues.scpinventory.network.InventoryActionPacket;
+import com.bl4ues.scpinventory.network.InventoryMovePacket;
 import com.bl4ues.scpinventory.network.KeyActionPacket;
 import com.bl4ues.scpinventory.network.ModNetwork;
 
@@ -25,5 +26,87 @@ public final class ClientInventoryBridge {
         }
 
         ModNetwork.CHANNEL.sendToServer(new EquipmentActionPacket(slot.name(), name));
+    }
+
+    public static void moveMainToMain(int sourceIndex, int targetIndex) {
+        ModNetwork.CHANNEL.sendToServer(new InventoryMovePacket(
+                InventoryMovePacket.PLACE_MAIN,
+                sourceIndex,
+                "",
+                InventoryMovePacket.PLACE_MAIN,
+                targetIndex,
+                ""
+        ));
+    }
+
+    public static void moveMainToEquipment(int sourceIndex, ScpEquipmentSlot targetSlot) {
+        if (targetSlot == null) {
+            return;
+        }
+
+        ModNetwork.CHANNEL.sendToServer(new InventoryMovePacket(
+                InventoryMovePacket.PLACE_MAIN,
+                sourceIndex,
+                "",
+                InventoryMovePacket.PLACE_EQUIPMENT,
+                -1,
+                targetSlot.name()
+        ));
+    }
+
+    public static void moveMainToWorld(int sourceIndex) {
+        ModNetwork.CHANNEL.sendToServer(new InventoryMovePacket(
+                InventoryMovePacket.PLACE_MAIN,
+                sourceIndex,
+                "",
+                InventoryMovePacket.PLACE_WORLD,
+                -1,
+                ""
+        ));
+    }
+
+    public static void moveEquipmentToMain(ScpEquipmentSlot sourceSlot, int targetIndex) {
+        if (sourceSlot == null) {
+            return;
+        }
+
+        ModNetwork.CHANNEL.sendToServer(new InventoryMovePacket(
+                InventoryMovePacket.PLACE_EQUIPMENT,
+                -1,
+                sourceSlot.name(),
+                InventoryMovePacket.PLACE_MAIN,
+                targetIndex,
+                ""
+        ));
+    }
+
+    public static void moveEquipmentToEquipment(ScpEquipmentSlot sourceSlot, ScpEquipmentSlot targetSlot) {
+        if (sourceSlot == null || targetSlot == null) {
+            return;
+        }
+
+        ModNetwork.CHANNEL.sendToServer(new InventoryMovePacket(
+                InventoryMovePacket.PLACE_EQUIPMENT,
+                -1,
+                sourceSlot.name(),
+                InventoryMovePacket.PLACE_EQUIPMENT,
+                -1,
+                targetSlot.name()
+        ));
+    }
+
+    public static void moveEquipmentToWorld(ScpEquipmentSlot sourceSlot) {
+        if (sourceSlot == null) {
+            return;
+        }
+
+        ModNetwork.CHANNEL.sendToServer(new InventoryMovePacket(
+                InventoryMovePacket.PLACE_EQUIPMENT,
+                -1,
+                sourceSlot.name(),
+                InventoryMovePacket.PLACE_WORLD,
+                -1,
+                ""
+        ));
     }
 }
