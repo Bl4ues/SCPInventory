86e550969121aed907df19e17cf851cf8c46a58d
Deyvid Campos <deyvid04campos@gmail.com>
2026-06-30 10:14:07 -0300

    Detect drinkable and potion items as consumables

--- src/main/java/com/bl4ues/scpinventory/item/ScpItemClassifier.java
+++ src/main/java/com/bl4ues/scpinventory/item/ScpItemClassifier.java
@@ -6,6 +6,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.UseAnim;
 
 import java.util.Optional;
 
@@ -29,7 +30,7 @@ public final class ScpItemClassifier {
             return configuredType.get();
         }
 
-        if (stack.isEdible()) {
+        if (isDefaultConsumable(stack)) {
             return ScpItemType.CONSUMABLE;
         }
 
@@ -71,6 +72,29 @@ public final class ScpItemClassifier {
         }
 
         return Optional.empty();
+    }
+
+    private static boolean isDefaultConsumable(ItemStack stack) {
+        if (stack.isEdible()) {
+            return true;
+        }
+
+        UseAnim animation = stack.getUseAnimation();
+        if (animation == UseAnim.EAT || animation == UseAnim.DRINK) {
+            return true;
+        }
+
+        ResourceLocation stackId = BuiltInRegistries.ITEM.getKey(stack.getItem());
+        if (stackId == null) {
+            return false;
+        }
+
+        String path = stackId.getPath();
+        return path.equals("potion")
+                || path.equals("splash_potion")
+                || path.equals("lingering_potion")
+                || path.endsWith("_potion")
+                || path.contains("potion");
     }
 
     private static Optional<ScpItemType> getConfiguredType(ItemStack stack) {
