0aa4479f0667929b61b6683f2f12531db23d8927
Deyvid Campos <deyvid04campos@gmail.com>
2026-06-30 12:58:44 -0300

    Prevent dropped SCP inventory items from merging

--- src/main/java/com/bl4ues/scpinventory/events/BlockPickupHandler.java
+++ src/main/java/com/bl4ues/scpinventory/events/BlockPickupHandler.java
@@ -1,12 +1,28 @@
 package com.bl4ues.scpinventory.events;
 
+import com.bl4ues.scpinventory.item.ScpPickupRouter;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.event.entity.EntityJoinLevelEvent;
 import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = "scpinventory")
 public class BlockPickupHandler {
+
+    @SubscribeEvent
+    public static void onEntityJoinLevel(EntityJoinLevelEvent event) {
+        if (event.getLevel().isClientSide()) {
+            return;
+        }
+
+        Entity entity = event.getEntity();
+        if (entity instanceof ItemEntity itemEntity && !itemEntity.getItem().isEmpty()) {
+            ScpPickupRouter.addNoMergeMarker(itemEntity.getItem(), itemEntity.getStringUUID());
+        }
+    }
 
     @SubscribeEvent
     public static void onItemPickup(EntityItemPickupEvent event) {
