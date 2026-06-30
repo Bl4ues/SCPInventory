7e243f0183a9933fe27e5a7cc3fbb5217d6fe774
Deyvid Campos <deyvid04campos@gmail.com>
2026-06-30 03:07:50 -0300

    Render pickup target world outline

---
+++ src/main/java/com/bl4ues/scpinventory/client/PickupPromptWorldEvents.java
@@ -0,0 +1,21 @@
+package com.bl4ues.scpinventory.client;
+
+import com.bl4ues.scpinventory.ScpInventoryMod;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RenderLevelStageEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = ScpInventoryMod.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
+public final class PickupPromptWorldEvents {
+
+    private PickupPromptWorldEvents() {
+    }
+
+    @SubscribeEvent
+    public static void renderPickupOutline(RenderLevelStageEvent event) {
+        if (event.getStage() == RenderLevelStageEvent.Stage.AFTER_ENTITIES) {
+            PickupPromptClient.renderWorldOutline(event.getPoseStack(), event.getCamera());
+        }
+    }
+}
