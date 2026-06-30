8d1ed8194604ed73b221edcb00d438dc67b0092c
Deyvid Campos <deyvid04campos@gmail.com>
2026-06-29 21:08:43 -0300

    Tick client stamina state

---
+++ src/main/java/com/bl4ues/scpinventory/client/ClientGameplayEvents.java
@@ -0,0 +1,20 @@
+package com.bl4ues.scpinventory.client;
+
+import com.bl4ues.scpinventory.ScpInventoryMod;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = ScpInventoryMod.MODID, value = Dist.CLIENT)
+public final class ClientGameplayEvents {
+
+    private ClientGameplayEvents() {
+    }
+
+    @SubscribeEvent
+    public static void onClientTick(TickEvent.ClientTickEvent event) {
+        if (event.phase != TickEvent.Phase.END) return;
+        PlayerVitalsClient.clientTick();
+    }
+}
