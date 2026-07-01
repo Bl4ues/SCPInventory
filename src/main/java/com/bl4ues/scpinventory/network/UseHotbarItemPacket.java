bf3e4664e05f56dca5b0e3c88d0cea4a37e6adcc
Deyvid Campos <deyvid04campos@gmail.com>
2026-07-01 15:09:44 -0300

    Send usable item stack to client activation

--- src/main/java/com/bl4ues/scpinventory/network/UseHotbarItemPacket.java
+++ src/main/java/com/bl4ues/scpinventory/network/UseHotbarItemPacket.java
@@ -2,6 +2,7 @@ package com.bl4ues.scpinventory.network;
 
 import com.bl4ues.scpinventory.client.ClientPacketHandlers;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.network.NetworkEvent;
@@ -12,25 +13,28 @@ public class UseHotbarItemPacket {
 
     private final int hotbarSlot;
     private final boolean continuousUse;
+    private final ItemStack stack;
 
-    public UseHotbarItemPacket(int hotbarSlot, boolean continuousUse) {
+    public UseHotbarItemPacket(int hotbarSlot, boolean continuousUse, ItemStack stack) {
         this.hotbarSlot = hotbarSlot;
         this.continuousUse = continuousUse;
+        this.stack = stack == null ? ItemStack.EMPTY : stack.copy();
     }
 
     public static void encode(UseHotbarItemPacket msg, FriendlyByteBuf buf) {
         buf.writeInt(msg.hotbarSlot);
         buf.writeBoolean(msg.continuousUse);
+        buf.writeItem(msg.stack);
     }
 
     public static UseHotbarItemPacket decode(FriendlyByteBuf buf) {
-        return new UseHotbarItemPacket(buf.readInt(), buf.readBoolean());
+        return new UseHotbarItemPacket(buf.readInt(), buf.readBoolean(), buf.readItem());
     }
 
     public static void handle(UseHotbarItemPacket msg, Supplier<NetworkEvent.Context> ctx) {
         ctx.get().enqueueWork(() ->
                 DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () ->
-                        ClientPacketHandlers.activateUsableItem(msg.hotbarSlot, msg.continuousUse)
+                        ClientPacketHandlers.activateUsableItem(msg.hotbarSlot, msg.continuousUse, msg.stack)
                 )
         );
         ctx.get().setPacketHandled(true);
