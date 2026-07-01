78ec94cd3de3b353dfe79573a00bd003e2104bc1
Deyvid Campos <deyvid04campos@gmail.com>
2026-07-01 11:58:27 -0300

    Add usable coin and offhand accessory item types

--- src/main/java/com/bl4ues/scpinventory/item/ScpItemType.java
+++ src/main/java/com/bl4ues/scpinventory/item/ScpItemType.java
@@ -6,10 +6,13 @@ import java.util.Optional;
 public enum ScpItemType {
     MISCELLANEOUS("Miscellaneous"),
     CONSUMABLE("Consumable"),
+    USABLE("Usable"),
     KEY("Key"),
     CODEX("Document"),
+    COIN("Coin"),
     HEAD("Head"),
     ACCESSORY("Accessory"),
+    ACCESSORY_HAND("Accessory"),
     CHEST("Chest"),
     LEGS("Legs"),
     FEET("Feet"),
@@ -32,7 +35,7 @@ public enum ScpItemType {
     public Optional<ScpEquipmentSlot> getEquipmentSlot() {
         return switch (this) {
             case HEAD -> Optional.of(ScpEquipmentSlot.HEAD);
-            case ACCESSORY -> Optional.of(ScpEquipmentSlot.ACCESSORY);
+            case ACCESSORY, ACCESSORY_HAND -> Optional.of(ScpEquipmentSlot.ACCESSORY);
             case CHEST -> Optional.of(ScpEquipmentSlot.CHEST);
             case LEGS -> Optional.of(ScpEquipmentSlot.LEGS);
             case FEET -> Optional.of(ScpEquipmentSlot.FEET);
@@ -48,11 +51,15 @@ public enum ScpItemType {
 
         return switch (token.trim().toUpperCase(Locale.ROOT)) {
             case "MISC", "MISCELLANEOUS", "MISCELLANEOUSLY" -> Optional.of(MISCELLANEOUS);
-            case "CONSUMABLE", "USE", "USABLE" -> Optional.of(CONSUMABLE);
+            case "CONSUMABLE", "CONSUME", "USE" -> Optional.of(CONSUMABLE);
+            case "USABLE", "USEABLE", "NON_CONSUMABLE", "NONCONSUMABLE", "RIGHT_CLICK", "RIGHTCLICK" -> Optional.of(USABLE);
             case "KEY", "KEYCARD", "KEYRING" -> Optional.of(KEY);
             case "CODEX", "DOCUMENT", "DOC" -> Optional.of(CODEX);
+            case "COIN", "CURRENCY", "TOKEN", "MONEY" -> Optional.of(COIN);
             case "HEAD", "HELMET", "MASK" -> Optional.of(HEAD);
             case "ACCESSORY", "TRINKET", "RING", "AMULET" -> Optional.of(ACCESSORY);
+            case "ACCESSORYHAND", "ACCESSORY_HAND", "ACCESSORYOFFHAND", "ACCESSORY_OFFHAND",
+                 "OFFHAND_ACCESSORY", "OFFHANDACCESSORY", "ACCESORYHAND", "ACCESORY_HAND" -> Optional.of(ACCESSORY_HAND);
             case "BODY", "CHEST", "CHESTPLATE", "TORSO" -> Optional.of(CHEST);
             case "LEGS", "LEGGINGS", "PANTS" -> Optional.of(LEGS);
             case "FEET", "BOOTS", "SHOES" -> Optional.of(FEET);
